854dc4cc149d5d6a7cbc6fc02b337541378c6b85
Jonathan <andriodjonathan1@gmail.com>
2020-07-03 14:11:02 -0400

    More comments

--- src/TodoList.js
+++ src/TodoList.js
@@ -5,7 +5,9 @@ import "./TodoList.css";
 class TodoList extends Component {
     constructor(props){
         super(props);
+        // Our intial state for our todo list 
         this.state = {
+        //Empty array to put our tasks into 
         items: []
         };
 
@@ -16,13 +18,36 @@ class TodoList extends Component {
     addItem(e) {
         // Creating the Item
         // If it is NOT blank then loop will start
+        if (this._inputElement.value != ""){
+            //new variable named newItem that stores a object
+            var newItem = {
+                text: this._inputElement.value,
+                //Key that stores a unique time
+                key: Date.now()
+            };
+
+            //State of items array getting previous state and concat the new items to it 
+            //prevState 
+            this.setState((prevState) => {
+                return{
+                    items: prevState.items.concat(newItem)
+                }
+            }
+
+
+            );
+
+        }
+
+        
+
         // Creates a new object named newItem and gives it a text key pair values
         // The value of the inputElement is stored in the text variable
         // The key helps us keep track of multiple items we are inputting. Gives us a precise number when we got our value
         
-           
+        // A form when submitted tries to send it somewhere do this prevents the page from reloading.  
         e.preventDefault();
-        console.log('form submitted!');
+        
       }
     render() {
         return (
